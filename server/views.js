b64b259530ca42f93eaddb70dc44812ee6359304
Adam Brault <adam@andyet.net>
2014-01-12 00:53:23 -0800

    more levelgraph

--- server/views.js
+++ server/views.js
@@ -1,6 +1,9 @@
-var levelgraph = require('levelgraph');
+var levelgraph  = require('levelgraph');
+var gravatar    = require('gravatar');
 
-var db = levelgraph('./levelgraph.db');
+var graphdb     = levelgraph('./levelgraph.db');
+
+var triple, list;
 
 exports.addPersonForm = function (request, reply) {
   reply.view('add', { foo: 'bar' });
@@ -22,29 +25,42 @@ exports.newPerson = function (request, reply) {
 
     var form = request.payload;
 
-    var emailTriple = { 
-      subject   : form.name, 
-      predicate : 'hasEmail',
-      object    : form.email
+    var img = gravatar.url(form.email, 100);
+
+    if (form.email) {
+      var emailTriple = { 
+        subject   : form.name, 
+        predicate : 'hasEmail',
+        object    : form.email
+      };
+      var gravatarTriple = {
+        subject   : form.name,
+        predicate : 'hasGravatar',
+        object    : img
+      }
     };
 
-    var twitterTriple = {
-      subject   : form.name,
-      predicate : 'hasTwitter',
-      object    : form.twitter
+    if (form.twitter) {
+      var twitterTriple = {
+        subject   : form.name,
+        predicate : 'hasTwitter',
+        object    : form.twitter
+      };
     };
+    
+    if (form.bio) {
+      var bioTriple = {
+        subject   : form.name,
+        predicate : 'hasBio',
+        object    : form.bio
+      };
+    }
 
-    var bioTriple = {
-      subject   : form.name,
-      predicate : 'hasBio',
-      object    : form.hasBio
-    };
+    var tripleSet = [ emailTriple, twitterTriple, bioTriple, gravatarTriple ];
 
-    var tripleSet = [ emailTriple, twitterTriple, bioTriple ];
-
-    db.put(tripleSet, function (err) {
-      if (err) return console.log('db error:', err);
-      console.log('triples:', emailTriple, twitterTriple, bioTriple);
+    graphdb.put(tripleSet, function (err) {
+      if (err) return console.log('levelgraph error:', err);
+      console.log('triples:', tripleSet);
     });
 
     reply(tripleSet).code(201).header('Location', '/people/' + form.name);
