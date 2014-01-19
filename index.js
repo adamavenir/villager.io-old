735451b6e324a12cbd6af628b2113da070221faa
Adam Brault <adam@andyet.net>
2014-01-19 15:10:44 -0800

    remove auth from index

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var Hapi      = require('hapi');
-var config    = require('getconfig');
 var jade      = require('jade');
 var routes    = require('./server/routes');
-var traverse  = require('traverse');
-var _         = require('underscore');
 
 var serverOptions = {
     views: {
@@ -12,30 +9,10 @@ var serverOptions = {
     }
 };
 
-var server = new Hapi.Server('localhost', config.http.port, serverOptions);
+var server = new Hapi.Server('localhost', 8000, serverOptions);
 
-var travelogue_config = config.auth;
-traverse(travelogue_config).forEach(function () {
-    if (this.key === 'callbackPath') {
-        this.parent.node.callbackURL = config.http.externalURL + this.node;
-    }
-});
+server.route(routes);
 
-server.pack.require({
-        yar: config.session,
-        travelogue: travelogue_config,
-    },
-    function pluginsCallback(err) {
-        var Passport = server.plugins.travelogue.passport;
-        if (err) throw err;
-        // add our login routes
-        server.route(routes(Passport));
+console.log('hapi listening on 8000');
 
-        server.start(function () {
-            console.log('triciti.es listening at:', server.info.uri);
-        });
-        server.on('internalError', function internalError(event) {
-            console.log('[server internalError]', event);
-        });
-    }
-);
+server.start();
