b3db907c0d55dc8957dd85865fd3b03183eb4c7d
Adam Brault <adam@andyet.net>
2014-01-18 20:18:47 -0800

    removed db, cleaned up views console.log

--- server/views.js
+++ server/views.js
@@ -28,22 +28,16 @@ exports.createPerson = function (request, reply) {
   });
   p.save(function (err) {
     Person.load(p.key, function (err, person) {
-      // console.log('person value:', person.toJSON());
-      // console.log('person key', person.key);
-      reply(p.key + JSON.stringify(person)).code(201).redirect('/people/' + p.slug);
+      reply().code(201).redirect('/people/' + p.slug);
     })
   });
 };
 
 exports.getPerson = function (request, reply) {
   var thisPerson = request.params.person;
-  console.log('Looking up key: "' + Person.options.prefix + thisPerson + '"');
   Person.load(Person.options.prefix + thisPerson, function(err, value) {
     if (err) {
-      console.log('get err:', err);
-      console.log('get val:', value);
       reply.view('404');
-      console.log('The key "' + Person.options.prefix + thisPerson + '" does not exist.')
     }
     else {
       reply.view('person', value);
@@ -53,14 +47,10 @@ exports.getPerson = function (request, reply) {
 
 exports.listPeople = function (request, reply) {
   Person.all(function(err, data) {
-    if(err) {
-      console.log('listPeople err:', err);
-    }
     if(data.length === 0) {
       reply.view('noPeople');
     }
     else {
-      console.log('listPeople data', JSON.stringify(data));
       reply.view('listPeople', { people : data});  
     }
   });
@@ -68,7 +58,6 @@ exports.listPeople = function (request, reply) {
 
 exports.delete = function (request, reply) {
   var key = request.params.prefix + '!' + request.params.person;
-  console.log('deleting', key);
   Person.delete(key, callback);
   var callback = reply.view('deleted').redirect('/' + request.params.prefix);
 };
