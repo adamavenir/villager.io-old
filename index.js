e80360c154ff66b4d20c63442755a180bda61a17
Adam Brault <adam@andyet.net>
2014-01-23 22:41:24 -0800

    wip twitter auth

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var routes  = require('./server/routes');
 var level   = require('level');
 var db      = level('./db', { valueEncoding: 'json' });
 var models = require('./server/models')
+var TwitterStrategy = require('passport-twitter').Strategy;
+var config = require('getconfig');
+
+
+var plugins = {
+    yar: {
+        cookieOptions: {
+            password: 'worldofwalmart',
+            isSecure: false
+        }
+    },
+    travelogue: '../../' // use '../../' instead of travelogue if testing this repo locally
+};
 
 models.attachDB(db);
 
@@ -14,10 +27,37 @@ var serverOptions = {
     }
 };
 
-var server = new Hapi.Server('localhost', 8000, serverOptions);
+var server = new Hapi.Server(config.hostname, config.port, serverOptions);
+server.pack.require(plugins, function(err) {
+  if (err) {
+    throw err;
+  }
+});
+
+var Passport = server.plugins.travelogue.passport;
+Passport.use(new TwitterStrategy(config.twitter, function(accessToken, refreshToken, profile, done) {
+
+  // TODO find or create user here..
+
+  return done(null, profile);
+}));
+
+Passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+Passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+if (process.env.DEBUG) {
+    server.on('internalError', function (event) {
+        console.log(event)
+    });
+};
 
 server.route(routes);
 
-console.log('hapi listening on 8000');
+console.log('hapi listening on ', config.port);
 
 server.start();
