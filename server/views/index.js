d4b0df5184fcb62aee5d77b1dc698afa2654c0e8
Adam Brault <adam@andyet.net>
2014-02-08 09:53:13 -0800

    cleanup

--- server/views/index.js
+++ server/views/index.js
@@ -1,13 +1,7 @@
 module.exports = function views(server) {
-
   auth = require('./auth')(server);
-
   moderation = require('./moderation')(server);
-
   pages = require('./pages')(server);
-
   people = require('./people')(server);
-
   places = require('./places')(server);
-
 }
